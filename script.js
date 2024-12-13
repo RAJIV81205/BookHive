474cc6caf5c907ac1d7b3638226734e34f3bbd3b
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2024-12-13 22:50:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const loader = document.getElementById("loader-container");
   loader.style.display = "none"
 
+  const current = window.location.pathname.split("/").pop();
+
+  if (current=="fictional.html"){
+    fetchFictional()
+  }
+
 
 });
-
-function scrollLeft() {
-  const genre = document.querySelector(".genre");
-  genre.scrollBy({ left: -300, behavior: "smooth" });
-}
-
-function scrollRight() {
-  const genre = document.querySelector(".genre");
-  genre.scrollBy({ left: 300, behavior: "smooth" });
-}
-
 
 
 let currentIndex = 0;
@@ -31,5 +26,58 @@ function nextSlide() {
     showSlide(currentIndex);
 }
 
-// Change slides every 3 seconds
 setInterval(nextSlide, 3000);
+
+
+
+
+
+async function fetchFictional() {
+  const container = document.getElementById("fictional-book-container");
+
+  try {
+    
+    const response = await fetch('books.json');
+    const books = await response.json();
+
+    
+    const fictionalBooks = books.filter(book => book.category === "Fictional");
+
+   
+    container.innerHTML = '';
+
+    
+    fictionalBooks.forEach(book => {
+      const bookElement = document.createElement('div');
+      bookElement.classList.add('book-container'); 
+      bookElement.setAttribute('onclick', 'getBook(this)');
+      bookElement.setAttribute('data-isbn', book.isbn); 
+
+      bookElement.innerHTML = `
+        <img src="${book.image}" alt="${book.name}" ">
+        <h4>${book.name}</h4>
+        <p><s>₹${book.original_price}</s>  ₹${book.price}</p>
+      `;
+      container.appendChild(bookElement);
+    });
+
+    console.log(fictionalBooks); 
+  } catch (error) {
+    console.error('Error fetching books:', error);
+  }
+}
+
+
+function getBook(element) {
+
+  window.location.href = "book.html"
+  const isbn = element.getAttribute('data-isbn');
+  console.log(`Book ISBN: ${isbn}`);
+  // Add your logic here to display book details or take further actions
+}
+
+
+
+
+
+
