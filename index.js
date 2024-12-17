808830b9cfe1b8c4ad8edb1fe08389e84976f000
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2024-12-17 12:02:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const cors = require('cors'); // Import CORS
+const cors = require('cors'); 
 const dotenv = require('dotenv');
+const helmet = require('helmet');
 const path = require('path');
 
 
 dotenv.config();
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 app.use(cors()); 
+app.use(helmet());
 
 app.use(express.static(path.join(__dirname)));
 
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -130,3 +132,7 @@ app.post('/verify', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+    res.send('Helmet is protecting your app!');
+  });
