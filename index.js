5e8ae776d4c78c5efa7fa22ac17e3995b248dfe5
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2024-12-18 17:56:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors'); 
 const dotenv = require('dotenv');
 const helmet = require('helmet');
-const path = require('path);
+const path = require('path');
 
 
 dotenv.config();
